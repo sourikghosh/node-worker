ce23f98c5b43d623ea0a7a325c4a55098d07ed65
Sourik Ghosh <sourikghosh31@gmail.com>
2021-01-16 23:46:31 +0530

    bug fixed

--- src/workerThread1.js
+++ src/workerThread1.js
@@ -1,8 +1,7 @@
 import { parentPort, workerData } from 'worker_threads'
 import { hash } from 'bcrypt'
-
+const hPassword = await hash(process.env.PASSWORD, workerData.salt_rounds)
 parentPort.on('message', async (parentMsg) => {
-    const hPassword = await hash(process.env.PASSWORD, workerData.salt_rounds)
     parentMsg.port.postMessage({ hashedPassword: hPassword })
 })
 parentPort.postMessage({ hashedPassword: hPassword })
