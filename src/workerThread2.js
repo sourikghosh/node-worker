fcd5fcf6b572e758bbff1a625dcda6dc4517da06
Sourik Ghosh <sourikghosh31@gmail.com>
2021-01-16 23:49:12 +0530

    minor bug fix

--- src/workerThread2.js
+++ src/workerThread2.js
@@ -1,9 +1,11 @@
 import { parentPort } from 'worker_threads'
 import { compare } from 'bcrypt'
 
-parentPort.on('message', async (parentMsg) => {
+parentPort.on('message', (parentMsg) => {
+
     parentMsg.port.on('message', async (workermsg) => {
         const result = await compare(process.env.PASSWORD, workermsg.hashedPassword)
+
         parentPort.postMessage({ result: result })
     })
 })
